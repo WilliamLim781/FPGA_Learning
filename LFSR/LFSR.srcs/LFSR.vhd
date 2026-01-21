79e6a78abc4d8038436288060acee6a95681e2b9
William Lim <113472670+WilliamLim781@users.noreply.github.com>
2026-01-20 21:39:48 -0600

    added the base file for an LFSR module

---
+++ LFSR/LFSR.srcs/LFSR.vhd
@@ -0,0 +1,16 @@
+library library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity LFSR is 
+    port(
+
+    );
+end LFSR;
+
+architecture Behavioral of LFSR is 
+
+begin
+
+end Behavioral;
+
